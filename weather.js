974e683785ebf2566a7e2ac9e10ef7c9445e99a0
gurdeep03 <124983079+gurdeep03@users.noreply.github.com>
2024-02-10 21:05:43 +0530

    Update weather.js

--- weather.js
+++ weather.js
@@ -1,13 +1,13 @@
-const apikey="e2bd78418baeed33d668165063807a61";
+const apikey="e2bd78418baeed33d668165063807a61"; //used to authenticate and access a specific API.
 window.addEventListener("load",()=>{
     if(navigator.geolocation){
-        navigator.geolocation.getCurrentPosition((position)=>{
+        navigator.geolocation.getCurrentPosition((position)=>{  //receives the user's position object containing latitude and longitude coordinates.
             let lon= position.coords.longitude;
             let lat= position.coords.latitude;
             const url= `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&` + `lon=${lon}&appid=${apikey}`;
             
 
-            fetch(url).then((res)=>{
+            fetch(url).then((res)=>{ // retrieves the weather data from the OpenWeatherMap API.
                 return res.json();
             }).then((data)=>{
                 console.log(data);
@@ -22,20 +22,20 @@ window.addEventListener("load",()=>{
 })
 
 
-function searchByCity(){
+function searchByCity(){  //retrieves the value entered in the input field
     var place= document.getElementById('input').value;
-    var urlsearch= `http://api.openweathermap.org/data/2.5/weather?q=${place}&` + `appid=${apikey}`;
-
-    fetch(urlsearch).then((res)=>{
+    var urlsearch= `http://api.openweathermap.org/data/2.5/weather?q=${place}&` + `appid=${apikey}`; //used to fetch weather data for the specified city.
+    //GET request to the constructed URL
+    fetch(urlsearch).then((res)=>{ // This converts the response data into a JavaScript object that can be easily used and manipulated.
         return res.json();
-    }).then((data)=>{
+    }).then((data)=>{  
         console.log(data);
         weatherReport(data);
     })
     document.getElementById('input').value='';
 }
 
-function weatherReport(data){
+function weatherReport(data){ //represents the weather data for a specific city
 
     var urlcast= `http://api.openweathermap.org/data/2.5/forecast?q=${data.name}&` + `appid=${apikey}`;
 
@@ -45,20 +45,21 @@ function weatherReport(data){
         console.log(forecast.city);
         hourForecast(forecast);
         dayForecast(forecast)
+        //These functions are responsible for processing and displaying the hourly and daily forecasts, respectively.
 
-        console.log(data);
+        console.log(data); //updates the HTML elements on the page with the relevant weather information
         document.getElementById('city').innerText= data.name + ', '+data.sys.country;
         console.log(data.name,data.sys.country);
     
         console.log(Math.floor(data.main.temp-273));
         document.getElementById('temperature').innerText= Math.floor(data.main.temp-273)+ ' °C';
     
-        document.getElementById('clouds').innerText= data.weather[0].description;
-        console.log(data.weather[0].description)
+        document.getElementById('clouds').innerText= data.weather[0].main;
+        console.log(data.weather[0].main)
         
-        const image =document.querySelector(".icon");
+        const image =document.querySelector(".icon"); // selects the appropriate weather icon based on the weather condition
         if (data.weather[0].main=="Clouds"){
-            image.src="clouds.png"
+            image.src="clouds.gif"
         } else if(data.weather[0].main=="Thunderstorm"){
             image.src="thunderstorm.png"
         }
@@ -82,7 +83,8 @@ function weatherReport(data){
 
 }
 
-function hourForecast(forecast){
+function hourForecast(forecast){  //This function is responsible for processing and displaying the hourly forecast.
+
     document.querySelector('.templist').innerHTML=''
     for (let i = 0; i < 5; i++) {
 
@@ -105,7 +107,7 @@ function hourForecast(forecast){
 
         let desc= document.createElement('p');
         desc.setAttribute('class','desc')
-        desc.innerText= forecast.list[i].weather[0].description;
+        desc.innerText= forecast.list[i].weather[0].main;
 
         hourR.appendChild(div);
         hourR.appendChild(desc)
@@ -113,7 +115,8 @@ function hourForecast(forecast){
 }
 }
 
-function dayForecast(forecast){
+function dayForecast(forecast){ //This functions is responsible for processing and displaying the daily forecast.
+
     document.querySelector('.weekF').innerHTML=''
     for (let i = 8; i < forecast.list.length; i+=8) {
         console.log(forecast.list[i]);
@@ -129,10 +132,10 @@ function dayForecast(forecast){
         temp.innerText= Math.floor((forecast.list[i].main.temp_max - 273))+ ' °C' + ' / ' + Math.floor((forecast.list[i].main.temp_min - 273))+ ' °C';
         div.appendChild(temp)
 
-        let description= document.createElement('p');
-        description.setAttribute('class','desc')
-        description.innerText= forecast.list[i].weather[0].description;
-        div.appendChild(description);
+        let main= document.createElement('p');
+        main.setAttribute('class','desc')
+        main.innerText= forecast.list[i].weather[0].main;
+        div.appendChild(main);
 
         document.querySelector('.weekF').appendChild(div)
     }
